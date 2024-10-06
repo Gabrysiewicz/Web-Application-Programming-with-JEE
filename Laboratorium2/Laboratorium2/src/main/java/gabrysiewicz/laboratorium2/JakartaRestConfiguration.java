9a323271a97559485a09020b6dd6e070767de98f
Kamil Gabrysiewicz <s95400@pollub.edu.pl>
2024-10-06 21:39:51 +0200

    First Lab

---
+++ Laboratorium2/Laboratorium2/src/main/java/gabrysiewicz/laboratorium2/JakartaRestConfiguration.java
@@ -0,0 +1,13 @@
+package gabrysiewicz.laboratorium2;
+
+import jakarta.ws.rs.ApplicationPath;
+import jakarta.ws.rs.core.Application;
+
+/**
+ * Configures Jakarta RESTful Web Services for the application.
+ * @author Juneau
+ */
+@ApplicationPath("resources")
+public class JakartaRestConfiguration extends Application {
+    
+}
