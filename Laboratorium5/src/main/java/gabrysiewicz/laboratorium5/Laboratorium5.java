d5f30cd244aff7dc7466494eeb1c36cc340691a0
Kamil Gabrysiewicz <s95400@pollub.edu.pl>
2024-10-07 21:31:36 +0200

    Zadanie 5.1

---
+++ Laboratorium5/src/main/java/gabrysiewicz/laboratorium5/Laboratorium5.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package gabrysiewicz.laboratorium5;
+
+/**
+ *
+ * @author kamil
+ */
+public class Laboratorium5 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Laboratorium5/src/main/java/gabrysiewicz/laboratorium5/Main.java
@@ -0,0 +1,13 @@
+package gabrysiewicz.laboratorium5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
+@EnableAutoConfiguration
+@ComponentScan({"controllers"})
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class, args);
+    }
+}
+
