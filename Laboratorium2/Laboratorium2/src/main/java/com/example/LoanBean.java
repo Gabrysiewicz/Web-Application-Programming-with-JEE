728c7065e99bd676601d8aaada0cfec59e5762c2
Kamil Gabrysiewicz <s95400@pollub.edu.pl>
2024-10-16 13:42:47 +0200

    Update: Lab2, more exception handlers

--- Laboratorium2/Laboratorium2/src/main/java/com/example/LoanBean.java
+++ Laboratorium2/Laboratorium2/src/main/java/com/example/LoanBean.java
@@ -6,7 +6,8 @@ public class LoanBean implements Serializable {
     private double kwota;  // Kwota pożyczki
     private double procentRoczny;  // Procent roczny
     private int liczbaRat;  // Liczba rat
-
+    private double base;
+    private double result;
     public LoanBean() {
     }
 
@@ -34,8 +35,29 @@ public class LoanBean implements Serializable {
         this.liczbaRat = liczbaRat;
     }
 
-    public double getRata() {
-        double p = (procentRoczny / 100) / 12;
-        return (kwota * p) / (1 - Math.pow(1 + p, -liczbaRat));
+    public double getRata() throws IllegalArgumentException {
+    if (kwota <= 0) {
+        throw new IllegalArgumentException("Kwota musi być wartością większą od 0.");
+    }
+    if (procentRoczny <= 0) {
+        throw new IllegalArgumentException("Procent roczny musi być wartością większą od 0.");
+    }
+    if (liczbaRat <= 0) {
+        throw new IllegalArgumentException("Liczba rat musi być wartością większą od 0.");
+    }
+
+    // Calculate monthly interest rate
+    double p = (procentRoczny / 100) / 12;
+    if (p == 0) {
+        throw new IllegalArgumentException("Miesięczny przyrost jest wartością mniejąszą\równą zeru.");
+    }
+
+    base = (1 - Math.pow(1 + p, -liczbaRat));
+    if(base == 0){
+        throw new IllegalArgumentException("Dzielenie przez zero");
+    }
+    result = ((kwota * p) / base);
+    
+    return result;
     }
 }
