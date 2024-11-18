62e54b8f82f86da35ee84e3b5bf0f41a53449c43
Kamil Gabrysiewicz <kamilgabrysiewicz60@gmail.com>
2024-11-18 19:10:42 +0100

    Lab6, Task 1 and 2

---
+++ Laboratorium 6 InteliJ/Laboratorium6/src/test/java/com/example/Laboratorium6/Laboratorium6ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Laboratorium6;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Laboratorium6ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
